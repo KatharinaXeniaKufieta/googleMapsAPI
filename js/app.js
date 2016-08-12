d51f21e11b2973ec7b2413a53eb8c0b1d7a716a9
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-11 20:04:49 -0400

    feat: Add infowindows to markers

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ var markers = [];
 // This global polygon variable is to ensure only ONE polygon is rendered
 var polygon = null;
 
+// Create placemarkers array to use in multiple functions to have control
+// over the number of places that show
+var placeMarkers = [];
+
 // Make sure this function is globally available
 var displayDirections = function() {};
 
@@ -28,6 +32,10 @@ function initMap() {
   var zoomAutocomplete = new google.maps.places.Autocomplete(document.getElementById('zoom-to-area-text'));
   // Bias the boundaries within the map for the zoom to area text
   zoomAutocomplete.bindTo('bounds', map);
+  // Create a searchbox in order to execute a places search
+  var searchBox = new google.maps.places.SearchBox(document.getElementById('places-search'));
+  // Bias the searchbox to within the bounds of the map
+  searchBox.setBounds(map.getBounds());
 
   var largeInfowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
@@ -94,7 +102,7 @@ function initMap() {
   map.fitBounds(bounds);
 
   document.getElementById('show-listings').addEventListener('click', showListings);
-  document.getElementById('hide-listings').addEventListener('click', hideListings);
+  document.getElementById('hide-listings').addEventListener('click', hideMarkers);
   document.getElementById('toggle-drawing').addEventListener('click', function() {
     toggleDrawing(drawingManager);
   });
@@ -104,6 +112,14 @@ function initMap() {
   document.getElementById('search-within-time').addEventListener('click', function() {
     searchWithinTime();
   });
+  // Listen for the event fired when the user selects a prediction from the
+  // picklist and retrieve more details for that place
+  searchBox.addListener('places_changed', function() {
+    searchBoxPlaces(this);
+  });
+  // Listen for the event fired when the user selects a prediction and
+  // clicks "go" more details for that place
+  document.getElementById('go-places').addEventListener('click', textSearchPlaces);
   // Add an event listener so that the polygon is captured, call the
   // seachWithinPolygon function. This will show the markers in the polygon,
   // and hide any outside of it.
@@ -112,7 +128,7 @@ function initMap() {
     // If there is, get rid of it and remove the markers
     if (polygon) {
       polygon.setMap(null);
-      hideListings();
+      hideMarkers(markers);
     }
     // Switching the drawing mode to the HAND (i.e. no longer drawing)
     drawingManager.setDrawingMode(null);
@@ -181,7 +197,7 @@ function initMap() {
   }
 
   // This function will loop through the listings and hide them all
-  function hideListings() {
+  function hideMarkers(markers) {
     for (var i = 0, max = markers.length; i < max; i++) {
       markers[i].setMap(null);
     }
@@ -269,7 +285,7 @@ function initMap() {
     if (address == '') {
       window.alert('You must enter an address.');
     } else {
-      hideListings();
+      hideMarkers(markers);
       // Use the distance matrix service to calculate the duration of the
       // routes between all our markers, and the destination address entered
       // by the user. Then put all the origins into an origin matrix.
@@ -356,7 +372,7 @@ function initMap() {
   // of the markers within the calculated distance. This will display
   // the route on the map.
   displayDirections = function(origin) {
-    hideListings();
+    hideMarkers(markers);
     var directionsService = new google.maps.DirectionsService;
     // Get the destination address from the user entered value
     var destinationAddress = document.getElementById('search-within-time-text').value;
@@ -380,6 +396,129 @@ function initMap() {
         });
       } else {
         window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  // This function fires when the user selects a searchbox picklist item.
+  // It will do a nearby search using the selected query string or place
+  function searchBoxPlaces(searchBox) {
+    hideMarkers(placeMarkers);
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      window.alert('We did not find any places matching that search!');
+    } else {
+      // For each place, get the icon, name and location
+      createMarkersForPlaces(places);
+    }
+  }
+
+  // This function fires when the user select "go" on the places search
+  // It will do a nearby search using the entered query string or place
+  function textSearchPlaces() {
+    var bounds = map.getBounds();
+    hideMarkers(placeMarkers);
+    var placesService = new google.maps.places.PlacesService(map);
+    placesService.textSearch({
+      query: document.getElementById('places-search').value,
+      bounds: bounds
+    }, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        createMarkersForPlaces(results);
+      }
+    });
+  }
+
+  // This funciton creates markers for each place found in either places
+  // search
+  function createMarkersForPlaces(places) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < places.length; i++) {
+      var place = places[i];
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(35, 35),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(15, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location,
+        id: place.place_id
+      });
+      // Create a single infowindow to be used with the place details
+      // information so that only one is open at once
+      var placeInfoWindow = new google.maps.InfoWindow();
+      // If a marker is clicked, do a place details search on it in the next
+      // function
+      marker.addListener('click', function() {
+        if (placeInfoWindow.marker == this) {
+          console.log("This infowindow already is on this marker!");
+        } else {
+          getPlacesDetails(this, placeInfoWindow);
+        }
+      })
+      placeMarkers.push(marker);
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    }
+    map.fitBounds(bounds);
+  }
+
+  // This is the PLACE DETAILS search - it's the most detailed so it's only
+  // executed when a marker is selected, indicating the user wants more
+  // details about that place
+  function getPlacesDetails(marker, infowindow) {
+    var service = new google.maps.places.PlacesService(map);
+    service.getDetails({
+      placeId: marker.id
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        // Set the marker property on this infowindow so it isn't created
+        // again
+        infowindow.marker = marker;
+        var innerHTML = '<div>';
+        if (place.name) {
+          innerHTML += '<strong>' + place.name + '</strong>';
+        }
+        if (place.formatted_address) {
+          innerHTML += '<br>' + place.formatted_address;
+        }
+        if (place.formatted_phone_number) {
+          innerHTML += '<br>' + place.formatted_phone_number;
+        }
+        if (place.opening_hours) {
+          innerHTML += '<br><br><strong>Hours:</strong><br>' +
+            place.opening_hours.weekday_text[0] + '<br>' +
+            place.opening_hours.weekday_text[1] + '<br>' +
+            place.opening_hours.weekday_text[2] + '<br>' +
+            place.opening_hours.weekday_text[3] + '<br>' +
+            place.opening_hours.weekday_text[4] + '<br>' +
+            place.opening_hours.weekday_text[5] + '<br>' +
+            place.opening_hours.weekday_text[6];
+        }
+        if (place.photos) {
+          innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+            {maxHeight: 100, maxWidth: 200}) + '">';
+        }
+        innerHTML += '</div>';
+        infowindow.setContent(innerHTML);
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow
+        // is closed.
+        infowindow.addListener('closeclick', function() {
+          infowindow.marker = null;
+        });
+      } else {
+        console.log('status is not OK')
       }
     });
   }
